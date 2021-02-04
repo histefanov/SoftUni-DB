08a57ce377233ce06a84c690419d83fe415f0f94
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-02-04 10:11:38 +0200

    add

---
+++ Indices and Data Aggregation/AverageInterest.sql
@@ -0,0 +1,8 @@
+SELECT
+	DepositGroup,
+	IsDepositExpired,
+	AVG(DepositInterest) AS AverageInterest
+FROM WizzardDeposits
+WHERE DepositStartDate > '1985-1-1'
+GROUP BY DepositGroup, IsDepositExpired
+ORDER BY DepositGroup DESC, IsDepositExpired
