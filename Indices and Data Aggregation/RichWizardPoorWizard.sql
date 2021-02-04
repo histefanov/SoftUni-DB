117ead41f0e429ff17e0e453cde3c7be1f62ef32
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-02-04 13:07:38 +0200

    add

--- Indices and Data Aggregation/RichWizardPoorWizard.sql
+++ Indices and Data Aggregation/RichWizardPoorWizard.sql
@@ -1,11 +1,4 @@
-SELECT 
-	SUM(d.[Difference])
-FROM
-	(SELECT
-		h.FirstName AS [Host Wizard],
-		h.DepositAmount AS [Host Wizard Deposit],
-		g.FirstName AS [Guest Wizard],
-		g.DepositAmount AS [Guest Wizard Deposit],
-		h.DepositAmount - g.DepositAmount AS [Difference]
-	FROM WizzardDeposits h
-		JOIN WizzardDeposits g ON g.Id = h.Id + 1) AS d
+SELECT
+	ABS(SUM(h.DepositAmount - g.DepositAmount)) AS [Difference]
+FROM WizzardDeposits h
+	JOIN WizzardDeposits g ON h.Id = g.Id + 1
