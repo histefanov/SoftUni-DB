60edf72a8f48dc3e56973ff2c645840387bc3d14
Hristiyan Stefanov <christian.stefanov@outlook.com>
2021-02-19 20:03:37 +0200

    add diablo db problems

--- Triggers and Transactions/WithdrawMoney.sql
+++ Triggers and Transactions/WithdrawMoney.sql
@@ -1,10 +1,12 @@
 CREATE PROC usp_WithdrawMoney (@AccountId INT, @MoneyAmount MONEY)
 AS
 	BEGIN TRANSACTION
-		IF @AccountId NOT IN (SELECT Id FROM ACCOUNTS)
+		IF @AccountId NOT IN (SELECT Id FROM Accounts)
 			THROW 50003, 'Invalid account ID', 1;
 		IF @MoneyAmount <= 0
 			THROW 50004, 'Withdrawal amount cannot be negative or zero.', 1;
+		IF @MoneyAmount > (SELECT Balance FROM Accounts WHERE Id = @AccountId)
+			THROW 50005, 'Insufficient funds', 1;
 
 		UPDATE Accounts
 		SET Balance -= @MoneyAmount
